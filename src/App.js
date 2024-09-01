2f3e890a21bf982b33b37c567393b8c59c79cd2b
Ebubechukwudavid <152161851+Ebubechukwudavid@users.noreply.github.com>
2024-09-01 02:32:42 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ import './App.css';
+import BMICalculator from './BMICalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <BMICalculator></BMICalculator>
+    </div>
+  );
+}
+
+export default App;
